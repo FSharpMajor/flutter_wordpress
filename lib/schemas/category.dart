28a4ad36a494163b4cc82ed1290fdd86afaf6b02
FSharpMajor <51091375+FSharpMajor@users.noreply.github.com>
2020-12-28 13:58:11 +0800

    fixed format exp on int parse

--- lib/schemas/category.dart
+++ lib/schemas/category.dart
@@ -46,7 +46,7 @@ class Category {
     }*/
     lLinks = json['_links'] != null ? new Links.fromJson(json['_links']) : null;
     thumbnailId = json['meta'] != null
-        ? (json['meta']['_pciwgas_cat_thumb_id'].isNotEmpty
+        ? (json['meta']['_pciwgas_cat_thumb_id'][0] != ""
             ? int.parse(json['meta']['_pciwgas_cat_thumb_id'][0])
             : null)
         : null;
